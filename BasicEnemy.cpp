53a651b99ac4ac41be257ecad81e1f7da6e5985e
Dust <dusthandler@gmail.com>
2018-04-17 16:16:46 +0200

    module enemies troubles updated

--- BasicEnemy.cpp
+++ BasicEnemy.cpp
@@ -1,19 +1,12 @@
 #include "Application.h"
 #include "BasicEnemy.h"
 #include "ModuleCollision.h"
-#include "Path.h"
 
 BasicEnemy::BasicEnemy(int x, int y) : Enemy(x, y)
 {
 
 	fly.PushBack({ 5, 72, 21, 22 });
 	fly.speed = 0.2f;
-
-	cookie.PushBack({ -2,0 }, 500);
-	cookie.PushBack({ -2,4 }, 100);
-	cookie.PushBack({ -2,0 }, 500);
-	cookie.PushBack({ -2,0 }, 500);
-
 
 	animation = &fly;
 
@@ -23,26 +16,4 @@ BasicEnemy::BasicEnemy(int x, int y) : Enemy(x, y)
 	original_pos = position;
 
 
-}
-
-void Enemy_Brown_Cookie::Move()
-{
-	/*if (going_up)
-	{
-	if (wave > 1.0f)
-	going_up = false;
-	else
-	wave += 0.05f;
-	}
-	else
-	{
-	if (wave < -1.0f)
-	going_up = true;
-	else
-	wave -= 0.05f;
-	}*/
-
-	position = original_pos + cookie.GetCurrentPosition();
-	/*position.y = int(float(original_y) + (25.0f * sinf(wave)));
-	position.x -= 1;*/
 }
