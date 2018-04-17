d3481b28d1a7276255ca4ed6e96eabe0f676e398
Dust <dusthandler@gmail.com>
2018-04-17 22:52:13 +0200

    Revert "Revert "revert""
    This reverts commit b96701e9832d58a02c816de20f636bb717440db9.

--- ModuleEnemies.h
+++ ModuleEnemies.h
@@ -2,12 +2,7 @@
 #define _MODULEENEMIES_H__
 
 #include "Module.h"
-<<<<<<< HEAD
 #include "Path.h"
-=======
-#include "BasicEnemy.h"
-#include "Oscilatory.h"
->>>>>>> 8ac76136c29a69e4439dd6295233b145c274fd96
 
 #define MAX_ENEMIES 100
 
@@ -15,7 +10,6 @@ enum ENEMY_TYPES
 {
 	NO_TYPE,
 	BASIC_ENEMY,
-<<<<<<< HEAD
 
 
 
@@ -25,9 +19,6 @@ enum ENEMY_MOVEMENT
 {
 	NO_MOVEMENT,
 	STAY
-=======
-	OSCILATORY
->>>>>>> 8ac76136c29a69e4439dd6295233b145c274fd96
 };
 
 class Enemy;
@@ -37,7 +28,6 @@ struct EnemyInfo
 	ENEMY_TYPES type = ENEMY_TYPES::NO_TYPE;
 	ENEMY_MOVEMENT movement = ENEMY_MOVEMENT::NO_MOVEMENT;
 	int x, y;
-	SDL_Texture* texture = nullptr;
 };
 
 class ModuleEnemies : public Module
@@ -53,11 +43,7 @@ public:
 	bool CleanUp();
 	void OnCollision(Collider* c1, Collider* c2);
 
-<<<<<<< HEAD
 	bool AddEnemy(ENEMY_TYPES type, int x, int y, ENEMY_MOVEMENT movement);
-=======
-	bool AddEnemy(ENEMY_TYPES type, int x, int y, SDL_Texture* texture = nullptr);
->>>>>>> 8ac76136c29a69e4439dd6295233b145c274fd96
 
 private:
 
@@ -68,9 +54,6 @@ private:
 	EnemyInfo queue[MAX_ENEMIES];
 	Enemy* enemies[MAX_ENEMIES];
 	SDL_Texture* sprites;
-
-	//more textures
-	SDL_Texture* enemy1Texture;
 };
 
 #endif // _MODULEENEMIES_H__
