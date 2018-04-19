f6d5677bd0063da1eba9ea7f8cb087333d2dc74a
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2018-04-19 23:12:43 +0200

    Oscilatory Animation 0.5

--- EnemyOscilatory.cpp
+++ EnemyOscilatory.cpp
@@ -5,7 +5,32 @@
 
 EnemyOscilatory::EnemyOscilatory(int x, int y, powerUpTypes type, SDL_Texture* sprite) : Enemy(x, y)
 {
-	waveAnim.PushBack({ 0,0,48,46 });
+	waveAnim.PushBack({ 0,0,48,45 });
+	waveAnim.PushBack({ 49,0,48,46 });
+	waveAnim.PushBack({ 96,0,47,46 });
+	waveAnim.PushBack({ 144,0,48,46 });
+	waveAnim.PushBack({ 192,0,48,46 });
+	waveAnim.PushBack({ 0,47,48,46 });
+	waveAnim.PushBack({ 48,47,48,46 });
+	waveAnim.PushBack({ 96,47,48,46 });
+	waveAnim.PushBack({ 144,47,48,46 });
+	waveAnim.PushBack({ 192,47,48,46 });
+	waveAnim.PushBack({ 0,95,48,46 });
+	waveAnim.PushBack({ 48,95,48,46 });
+	waveAnim.PushBack({ 96,95,50,47 });
+	waveAnim.PushBack({ 146,95,48,49 });
+	waveAnim.PushBack({ 96,95,50,47 });
+	waveAnim.PushBack({ 48,95,48,46 });
+	waveAnim.PushBack({ 0,95,48,46 });
+	waveAnim.PushBack({ 192,47,48,46 });
+	waveAnim.PushBack({ 144,47,48,46 });
+	waveAnim.PushBack({ 96,47,48,46 });
+	waveAnim.PushBack({ 48,47,48,46 });
+	waveAnim.PushBack({ 0,47,48,46 });
+	waveAnim.PushBack({ 192,0,48,46 });
+	waveAnim.PushBack({ 144,0,48,46 });
+	waveAnim.PushBack({ 96,0,47,46 });
+	waveAnim.PushBack({ 49,0,48,46 });
 	waveAnim.speed = 0.2f;
 
 	animation = &waveAnim;
