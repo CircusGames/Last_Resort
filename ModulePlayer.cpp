8b65d327165ecfe0f6d6b1764faac30bbe6fdba3
peterMcP <36275626+peterMcP@users.noreply.github.com>
2018-04-19 22:36:55 +0200

    fixed lives for next gameLoops

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -339,7 +339,11 @@ update_status ModulePlayer::Update()
 			current_animation->finish = false;
 			destroyed = false;
 			if (lives <= 0)
+			{
+				//resets lives counter for next gameLoop
+				lives = 3;
 				App->fade->FadeToBlack((Module*)App->scene_lvl1, (Module*)App->gameOverScreen);
+			}
 			else
 			{
 				App->fade->FadeToBlack((Module*)App->scene_lvl1, (Module*)App->scene_lvl1); 
