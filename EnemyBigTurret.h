5266c12f9a57c638fa7e5f78c2f6d2eddd04e77e
Rafa <rafakanex@gmail.com>
2018-04-21 00:13:44 +0200

    TANK WIP

---
+++ EnemyBigTurret.h
@@ -0,0 +1,24 @@
+/*#ifndef _ENEMYBIGTURRET_H__
+#define _ENEMYBIGTURRET_H__
+
+#include "Enemy.h"
+#include "Path.h"
+#include "ModuleTextures.h"
+
+class EnemyBigTurret : public Enemy
+{
+private:
+
+	Path path;
+	Animation rotateAnim;
+	iPoint originalPos;
+
+	int original_x = NULL;
+
+public:
+
+	EnemyBigTurret(int x, int y, powerUpTypes, SDL_Texture* sprite = nullptr);
+	void Move();
+};
+
+#endif // !_ENEMYSMALLTURRET_H__*/
