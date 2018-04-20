2c6d82391fe90cb29ee7916b59caa8eb40333af5
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2018-04-21 00:30:35 +0200

    Starting Game Module Sequence

--- ModuleGameOver.h
+++ ModuleGameOver.h
@@ -17,9 +17,28 @@ public:
 	bool CleanUp();
 
 public:
-
 	SDL_Texture* gameOverTexture = nullptr;
+	SDL_Texture* blackScreenTexture = nullptr;
+	SDL_Rect gameOverFirstRect;
+	SDL_Rect gameOverMidRect;
 	SDL_Rect gameOverRect;
+
+	Uint32 now;
+	float start_time;
+	float total_time;
+	float lastFadeFromWhiteTime;
+	int timeInWhite;
+
+private:
+
+	enum intro_step
+	{
+		firstSecuence,
+		fade_to_white,
+		time_in_white,
+		fade_from_white,
+		press_start_screen,
+	} current_step = intro_step::firstSecuence;
 };
 
 #endif //__MODULEGAMEOVER_H__
