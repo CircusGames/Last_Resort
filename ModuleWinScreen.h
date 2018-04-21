95f066291ef4f2f17ecd853e760fb1b649ed9fed
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2018-04-21 22:25:04 +0200

    UI and Highscores Sprites Added to Assets+WinScreen Module Started

--- ModuleWinScreen.h
+++ ModuleWinScreen.h
@@ -1,5 +1,25 @@
-/*#ifndef _MODULEWINSCREEN_H__
+#ifndef _MODULEWINSCREEN_H__
 #define _MODULEWINSCREEN_H__
 
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
 
-#endif _MODULEWINSCREEN_H__*/
+/*class ModuleWinScreen : public Module
+{
+public:
+
+	ModuleWinScreen();
+	~ModuleWinScreen();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+	SDL_Texture* WinScreenTexture = nullptr;
+private:
+
+};*/
+
+#endif _MODULEWINSCREEN_H__
