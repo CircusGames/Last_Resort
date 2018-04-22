e7a64215081d399105ca0055fee45df5dcb5e47d
peterMcP <36275626+peterMcP@users.noreply.github.com>
2018-04-22 16:27:48 +0200

    fixed sfx UNIT

--- ModulePlayerUnit.cpp
+++ ModulePlayerUnit.cpp
@@ -194,8 +194,8 @@ bool ModulePlayerUnit::Start()
 	//playerUnitOrange = App->textures->Load("assets/Graphics/Player/orangeUnit.png");
 
 	//Audio
-	App->audio->LoadAudio("assets/Audio/SFX/player/Fix_Unit.wav", "Fix Unit", SFX);
-	App->audio->LoadAudio("assets/audio/SFX/player/Unfix_Unit.wav", "Unfix Unit", SFX);
+	App->audio->LoadAudio("assets/Audio/SFX/player/Fix_Unit.wav", "UnitLocked", SFX);
+	App->audio->LoadAudio("assets/audio/SFX/player/Unfix_Unit.wav", "UnitUnlocked", SFX);
 
 	//delta calculations
 	orbitSpeed = 1.0f;
@@ -218,11 +218,11 @@ update_status ModulePlayerUnit::PreUpdate()
 	{
 		if (unitLocked) {
 			unitLocked = false;
-			App->audio->ControlAudio("Fix Unit", SFX, PLAY);
+			App->audio->ControlAudio("UnitUlocked", SFX, PLAY);
 		}
 		else {
 			unitLocked = true;
-			App->audio->ControlAudio("Unfix Unit", SFX, PLAY);
+			App->audio->ControlAudio("UnitLocked", SFX, PLAY);
 		}
 	}
 
