0437439fafc64f964eb7cc86bda3ed7031c1ff71
peterMcP <36275626+peterMcP@users.noreply.github.com>
2018-05-22 00:12:36 +0200

    added cave foreground to postUpdate of the scenelvl3

--- ModuleSceneLvl3.cpp
+++ ModuleSceneLvl3.cpp
@@ -236,7 +236,7 @@ update_status ModuleSceneLvl3::Update()
 	}*/
 	
 	//cave background
-	App->render->Blit(fgTexture, 2150, 94, &fgRect, 0.50f);
+	//App->render->Blit(fgTexture, 2150, 94, &fgRect, 0.50f);
 	
 	//DEBUG actual colliders in scene
 	//LOG("current scene colliders: %d", App->collision->actualColliders);
@@ -246,10 +246,15 @@ update_status ModuleSceneLvl3::Update()
 
 update_status ModuleSceneLvl3::PostUpdate()
 {
+	//sea waves animations (foreground waves)
+
 	for (int i = 0; i < MAXWAVES; ++i)
 	{
 		App->render->Blit(fgWavesTexture, i * 128, 186, &seaWavesAnim[i].GetCurrentFrame(), 0.50f);
 	}
+
+	//cave background
+	App->render->Blit(fgTexture, 2150, 94, &fgRect, 0.50f);
 
 	return UPDATE_CONTINUE;
 }
