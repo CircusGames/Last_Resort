b4da8f6b134aa9853b8f526c320e48cd3d242612
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2018-05-23 11:04:31 +0200

    MiniTank SpriteSheet+Animation Damage Added

--- EnemyMiniTank.cpp
+++ EnemyMiniTank.cpp
@@ -3,15 +3,27 @@
 #include "ModuleCollision.h"
 #include "Globals.h"
 #include "ModuleEnemies.h"
+#include "SDL\include\SDL_timer.h"
 
 Enemy_MiniTank::Enemy_MiniTank(int x, int y, powerUpTypes type, SDL_Texture* thisTexture) : Enemy(x, y)
 {
 	enemyTex = thisTexture;
 
-	move.PushBack({ 0,0,61,47 });
+	move2.PushBack({ 0,1,61,50 });
+	move.PushBack({ 61,0,61,50 });
+	move.PushBack({ 122,0,61,48 });
+	move.speed = 0.2f;
+	
+	damage2.PushBack({ 0,62,61,50 });
+	damage.PushBack({ 61,61,61,50 });
+	damage.PushBack({ 122,61,61,48 });
+	damage.speed = 0.2f;
+
 	animation = &move;
 	shootanimation = &move2;
+	shootanimationdamage = &damage2;
 
+	current_animation = &move;
 	//Path
 	path.PushBack({ -0.1f, 0.0f }, 140, &move);
 	path.PushBack({ 1.1f, 0.0f }, 130, &move);
@@ -43,4 +55,26 @@ Enemy_MiniTank::Enemy_MiniTank(int x, int y, powerUpTypes type, SDL_Texture* thi
 void Enemy_MiniTank::Move()
 {
 	position = original_pos + path.GetCurrentSpeed(&animation);
+}
+
+void Enemy_MiniTank::Draw()
+{
+	if (receiveDamage)
+	{
+		current_frame = current_animation->current_frame;
+		current_animation = &damage;
+		current_animation->current_frame = current_frame;
+		receiveDamage = false;
+		start_damage_time = SDL_GetTicks();
+	}
+
+	nowDamagetime = SDL_GetTicks() - start_damage_time;
+
+	if (nowDamagetime > damageAnimTime)
+	{
+		current_frame = current_animation->current_frame;
+		current_animation = &move;
+		current_animation->current_frame = current_frame;
+	}
+
 }
--- EnemyMiniTank.h
+++ EnemyMiniTank.h
@@ -10,14 +10,21 @@ class Enemy_MiniTank : public Enemy
 private:
 	Path path;
 	iPoint original_pos;
-	Animation move, move2;
+	Animation move, move2, damage, damage2;
 	Animation * shootanimation;
+	Animation* shootanimationdamage;
+	Animation * current_animation;
+	Uint32 start_damage_time;
+	Uint32 nowDamagetime;
+	Uint32 damageAnimTime;
+	float current_frame;
 
 public:
 
 	Enemy_MiniTank(int x, int y, powerUpTypes type, SDL_Texture * thisTexture = nullptr);
 
 	void Move();
+	void Draw();
 };
 
 
